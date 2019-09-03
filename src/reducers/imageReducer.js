7a75a3418e92c51e9f60d46e836b771a5997f392
Jackmc <jackmc1122@gmail.com>
2019-09-03 09:44:39 -0700

    Add base redux files

---
+++ src/reducers/imageReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  loading: false,
+  error: null,
+  image: ''
+};
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import image from './imageReducer';
+
+export default combineReducers({
+  image
+});
