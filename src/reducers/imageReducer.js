b16eaa6b64c2f082559bcffb84adda8ee1014091
Jackmc <jackmc1122@gmail.com>
2019-09-03 10:39:18 -0700

    Default date to the current date

--- src/reducers/imageReducer.js
+++ src/reducers/imageReducer.js
@@ -1,10 +1,17 @@
 import { UPDATE_DATE } from '../actions/imageActions';
 
+let today = new Date();
+let dd = today.getDate();
+
+let mm = today.getMonth() + 1; 
+let yyyy = today.getFullYear();
+let date = `${yyyy}-${mm}-${dd}`;
+
 const initialState = {
   loading: false,
   error: null,
   image: '',
-  date: ''
+  date: date
 };
 
 export default function(state = initialState, action) {
