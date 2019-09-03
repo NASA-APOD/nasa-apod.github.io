8f170d282cd26a861dfcd1c59eb2fa5e70ce1292
Jackmc <jackmc1122@gmail.com>
2019-09-03 13:57:40 -0700

    Update url

--- src/services/imageApi.js
+++ src/services/imageApi.js
@@ -1,5 +1,5 @@
 export const getImage = (date) => {
-  return fetch('http://localhost:7891/api/v1/apod', {
+  return fetch('http://api-nasa-apod.herokuapp.com/api/v1/apod', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -9,7 +9,7 @@ export const getImage = (date) => {
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch image';
-      
+
       return json;
     });
 };
